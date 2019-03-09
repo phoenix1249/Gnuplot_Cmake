a475f7a0dbc4a8a5d88236c0688d5d59d53e54e9
phoenix1249 <maxime.do@ensta-bretagne.org>
2019-03-09 12:02:35 +0100

    V0.1 pour presentation de Lundi

--- exemple.cpp
+++ exemple.cpp
@@ -11,9 +11,10 @@ int main(int argc, char **argv){
     plt.set_y_limit(-10,10);
     for (int i = 0;i<360;i++){
         plt.clear();
-        //plt.draw_tank(1,0.5,i,"'blue'");
+        plt.draw_tank(1,0.5,i,"'blue'");
+        plt.draw_circle(-2,1,5,"'red'");
         plt.draw_sailboat(0,0,i,0.1,0.2,40,1);
-        usleep(100000);
+        //usleep(100000);
         plt.show();
     }
     return 0;
--- include/roblib.h
+++ include/roblib.h
@@ -1,46 +1,10 @@
 #include <Eigen/Dense>
 #include "gnuplot-iostream.h"
 #include <boost/tuple/tuple.hpp>
-#include <vector>
-#include <string>
-#include <map>
-
-#define PI 3.14159265
-typedef std::pair<double, double> point;
-
-
-class Plot
-{
-private:
-    Gnuplot gp;
-    std::vector<std::vector<point>> points;
-    std::vector<std::string> colors;
-    double x_min;
-    double x_max;
-    double y_min;
-    double y_max;
+#include "Plot.h"
 
 
 
-public:
-    Plot();
-    ~Plot();
-    void set_x_limit(double xmin, double xmax);
-    void set_y_limit(double ymin, double ymax);
-    void clear();
-    void draw_circle(point Center,double radius,std::string color);
-    void draw_circle(double x,double y,double radius,std::string color);
-    void draw_ellipse(point Center,double a, double b,std::string color);
-    void draw_ellipse(double x,double y,double a, double b,std::string color);
-    void plot(std::vector<point> & p,std::string color);
-    void plot(Eigen::MatrixXd & M,std::string color);
-    void draw_tank(double x,double y,double theta,std::string color,double r);
-    void draw_tank(double x,double y,double theta,std::string color);
-    void draw_disk(double x, double y,double radius,std::string color);//TODO
-    void draw_sailboat(double x,double y,double theta,double deltas,double deltar,double psi,double awind);
-    void draw_car(double x,double y,double theta,std::string color);//TODO
-    void show();
-};
 
 
 
